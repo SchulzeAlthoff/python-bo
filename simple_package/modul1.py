75c4d073622ff5d513044c0ce43d639997569076
SchulzeAlthoff <jan.schulze-althoff@hs-bochum.de>
2019-10-29 13:34:09 +0100

    First Juypter Sheet

--- simple_package/modul1.py
+++
@@ -1,5 +0,0 @@
-""" A modul to demonstrate the package concept """
-
-def func1():
-    """ A testfunction """
-    print("Greetings from modul1")
--- simple_package/modul2.py
+++
@@ -1,5 +0,0 @@
-""" A modul to demonstrate the package concept """
-
-def func2():
-    """ A testfunction """
-    print("Greetings from modul2")
